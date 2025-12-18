2071fbbd3524b0d8f0f50abddc3670f73dca9423
Álvaro Mozo Gaspar <ciencias8000@gmail.com>
2025-12-18 16:31:53 +0100

    Cambios en la tabla usuarios

--- usuarios.sql
+++ usuarios.sql
@@ -3,14 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-12-2025
+-- Tiempo de generación: 18-12-2025
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -30,14 +29,12 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `usuarios`;
 
 CREATE TABLE `usuarios` (
-  `coduser` int(11) NOT NULL AUTO_INCREMENT,
-  `idusuario` varchar(20) NOT NULL,
+  `coduser` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `idusuario` varchar(20) NOT NULL UNIQUE,
   `password` varchar(255) NOT NULL,
   `nombre` varchar(30) NOT NULL,
   `apellidos` varchar(50) NOT NULL,
-  `admitido` tinyint(1) NOT NULL DEFAULT 1,
-  PRIMARY KEY (`coduser`),
-  UNIQUE KEY `idusuario` (`idusuario`)
+  `admitido` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -45,9 +42,9 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`idusuario`, `password`, `nombre`, `apellidos`, `admitido`) VALUES
-('admin', '$2y$10$V9VZT7wD2N/c1h5lB4Iq7OTVQk6YxHtUCzC3.4EKv9lX/.N3PR6g6', 'Administrador', 'Principal', 1),
-('Alvaro_MG64', '$2y$10$0eXbN5eQ0PmlvWZqsi9mBuXKg5pFvMG6vPTM1XqKkNYMiM7s8gEB2', 'ALVARO', 'MOZO GASPAR', 1),
-('Zazza_I5', '$2y$10$G1KfLJdP8JX1e5Q2kRrj8eHcD1sF6r5oF7uBzVwZf8gS6XvHq1/7K', 'FEDERICO', 'ZOMPICCHIATTI', 1);
+('admin', '$2y$10$Kk4Yc.3b5W7pRvn0bRswEOGd05y9Zx9hQYFZptnZ2v6R.HGZzGZSO', 'Administrador', 'Principal', 1),
+('Alvaro_MG64', '$2y$10$Q8Ue8iQwC3vbtN1m8bWJ9e2n.8f0KpJHf6wT2mN8dR5JkL7fTQWfa', 'ALVARO', 'MOZO GASPAR', 1),
+('Zazza_I5', '$2y$10$N3pF8bW8Qv1kL2rXh6sZ7eP9y8R5jT2v6xF9hL7cY1kM4qVbTz3O', 'FEDERICO', 'ZOMPICCHIATTI', 1);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
